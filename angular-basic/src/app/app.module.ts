731c50529dcd1db205f15dd7f37a8494aace4c73
sohailnazar <nazar@mailcohen.com>
2019-10-20 23:50:45 +0300

    2. property binding

--- angular-basic/src/app/app.module.ts
+++ angular-basic/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    InterpolationComponent
+    InterpolationComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule,
