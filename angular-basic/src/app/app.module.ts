59e6708c60c1119e9eec3da1ce162951c2324c0a
sohailnazar <nazar@mailcohen.com>
2019-10-22 17:56:03 +0300

    15. angular routes button click

--- angular-basic/src/app/app.module.ts
+++ angular-basic/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
@@ -16,10 +16,11 @@ import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { ComponentInteractionComponent } from './component-interaction/component-interaction.component';
 import { PipesComponent } from './pipes/pipes.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
+
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { EmployeeService } from './employee.service';
 import { HttpClientModule } from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
@@ -37,8 +38,8 @@ import { HttpClientModule } from "@angular/common/http";
     NgForComponent,
     ComponentInteractionComponent,
     PipesComponent,
-    EmployeeListComponent,
-    EmployeeDetailComponent
+    EmployeeDetailComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
