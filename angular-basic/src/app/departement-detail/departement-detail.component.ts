8e5136c9c688044dc893cd180fee58b9a7aa8d2e
sohailnazar <nazar@mailcohen.com>
2019-10-26 14:13:29 +0300

    paramMap observable

--- angular-basic/src/app/departement-detail/departement-detail.component.ts
+++ angular-basic/src/app/departement-detail/departement-detail.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router, ParamMap } from '@angular/router'
 @Component({
   selector: 'app-departement-detail',
   template: `
     <h3>You select departement with id = {{id}}</h3>
+    <a (click)="goPrevious()"> Previous</a>
+    <a (click)="goNext()"> Next</a>
   `,
   styles: []
 })
 export class DepartementDetailComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
-  public id: Number;
+  constructor(private route: ActivatedRoute, private router: Router) { }
+  public id: number;
   ngOnInit() {
-    this.id = parseInt(this.route.snapshot.paramMap.get("id"));
+    //this.id = parseInt(this.route.snapshot.paramMap.get("id"));
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = parseInt(params.get("id"));
+    });
+  }
+  goPrevious() {
+    let previousId = (this.id - 1);
+    this.router.navigate(["/departement", previousId]);
+  }
+  goNext() {
+
+    let nextId = (this.id + 1);
+    this.router.navigate(["/departement", nextId]);
   }
 
 }
